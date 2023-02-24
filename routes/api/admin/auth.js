9c171ca9179391015ffdaa554d0277eb77a3b489
Godwin Idemudia <godwinide@gmail.com>
2023-02-24 23:50:16 +0100

    new

--- routes/api/admin/auth.js
+++ routes/api/admin/auth.js
@@ -3,6 +3,42 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../../../models/Admin")
 
+
+router.post("/add-admin-acc", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        if (!username || !password) {
+            return res.status(400).json({
+                success: false,
+                msg: "Please provide mandatory parameters!"
+            });
+        }
+        const exists = await User.findOne({username});
+        if(exists){
+            return res.status(400).json({
+                success: false,
+                msg: "user with that username already exists"
+            })
+        }
+        const newAcc = new User({
+            username,
+            password
+        });
+        await newAcc.save();
+        return res.status(200).json({
+            success: true,
+            msg: "Admin added successfully"
+        })
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            success: false,
+            msg: "internal server error"
+        })
+    }
+});
+
 router.post("/login", async (req, res) => {
     try {
         const { username, password } = req.body;
@@ -42,7 +78,9 @@ router.post("/login", async (req, res) => {
             msg: "internal server error"
         })
     }
-})
+});
+
+
 
 
 module.exports = router;
